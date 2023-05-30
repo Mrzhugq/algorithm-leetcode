631587f0f342b915c9e2778434651fa8166dfa14
sutong <2081140153@qq.com>
2023-05-30 21:50:33 +0800

    23-5-30

---
+++ java/linked/IntersectionOfTwoLinkedListsLcci.java
@@ -0,0 +1,45 @@
+package linked;
+
+/**
+ * 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表没有交点，返回 null 。
+ */
+public class IntersectionOfTwoLinkedListsLcci {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode curA = headA;
+        ListNode curB = headB;
+        int lenA = 0, lenB = 0;
+        // 求长度
+        while (curA != null) {
+            curA = curA.next;
+            lenA++;
+        }
+        while (curB != null) {
+            curB = curB.next;
+            lenB++;
+        }
+        // 让curA为最长链表的头
+        curA = headA;
+        curB = headB;
+        if (lenA < lenB) {
+            int temp1 = lenA;
+            lenA = lenB;
+            lenB = temp1;
+            ListNode temp2 = curA;
+            curA = curB;
+            curB = temp2;
+        }
+        // 长度差
+        int gap = lenA - lenB;
+        while (gap-- != 0) {
+            curA = curA.next;
+        }
+        while (curA != null) {
+            if (curA == curB) {
+                return curA;
+            }
+            curA = curA.next;
+            curB = curB.next;
+        }
+        return null;
+    }
+}
--- java/linked/RemoveNthNodeFromEndOfList_19.java
+++ java/linked/RemoveNthNodeFromEndOfList_19.java
@@ -5,7 +5,18 @@ package linked;
  */
 public class RemoveNthNodeFromEndOfList_19 {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        
-        return null;
+        ListNode dummyHead = new ListNode(0, head);
+        ListNode slow = dummyHead;
+        ListNode fast = dummyHead;
+        while (n-- != 0 && fast != null) {
+            fast = fast.next;
+        }
+        fast = fast.next; // 走n + 1步（这样才能让slow指向删除节点的上一个节点）
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummyHead.next;
     }
 }
